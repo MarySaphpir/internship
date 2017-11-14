624f381bc13b5af1a3e96f05b1663ccf4df41eeb
Mary <marysaphpir@gmail.com>
2017-11-14 09:29:59 +0200

    tests for promise and recurtion funstions

--- task1/test/test.js
+++ task1/test/test.js
@@ -1,34 +1,65 @@
-describe("sum", function() {
+let chai = require('chai');
+let expect = require('chai').expect;
+let chaiAsPromised = require('chai-as-promised');
+let getFibonachi = require('../modulePatern');
 
-    describe("возвращает сумму трех чисел", function() {
+chai.use(chaiAsPromised);
 
-        it("возвращает 2 + 2 + 2 = 6", function() {
-            assert.equal(sum(2)(2)(2), 6);
+describe("Testing promise", function() {
+
+    describe("Entering number should return number of Fibonachi", function() {
+        it('3 should be equal 2', function () {
+            let result = getFibonachi.fibonachiWithPromice(3);
+            return expect(result).to.eventually.equal(2).and.to.be.finite;
         });
 
-        it("возвращает 2 + 6 + 8 = 16", function() {
-            assert.equal(sum(2)(6)(8), 16);
+        it('6 should be equal 8', function () {
+            let result = getFibonachi.fibonachiWithPromice(6);
+            return expect(result).to.eventually.equal(8);
         });
-    })
+    });
 
-    describe("возвращает сумму трех дробных чисел", function() {
-
-        it("возвращает 2.5 + 2 + 2 = 6.5", function() {
-            assert.equal(sum(2.5)(2)(2), 6.5);
+    describe("Entering a negative number, should return NaN", function() {
+        it('-6 should be return NaN', function () {
+            let result = getFibonachi.fibonachiWithPromice(-6);
+            return expect(result).to.be.NaN;
         });
+    });
 
-        it("возвращает 2 + 6.3 + 8.8 = 17.1", function() {
-            assert.equal(sum(2)(6.3)(8.8), 17.1);
+    describe("Entering a fractional number, should return NaN", function() {
+        it('6.5 should be return NaN', function () {
+            let result = getFibonachi.fibonachiWithPromice(6.5);
+            return expect(result).to.be.NaN;
         });
-    })
-
-    describe("если передана строка в качесте аргумента вернет NaN", function() {
-
-        it("возвращает 2.5 + 2 + 2 = 6.5", function() {
-            assert(isNaN(sum(2.5)('2')(2)));
-        });
-
-    })
-
+    });
 });
 
+
+describe("Testing function", function() {
+
+    describe("Entering number should return number of Fibonachi", function() {
+        it('3 should be equal 2', function () {
+            let result = getFibonachi.fibonachiWithRecurtion(3);
+            return expect(result).to.be.equal(2).and.to.be.finite;
+        });
+
+        it('6 should be equal 8', function () {
+            let result = getFibonachi.fibonachiWithRecurtion(6);
+            return expect(result).to.be.equal(8);
+        });
+    });
+
+    describe("Entering a negative number, should return NaN", function() {
+        it('-6 should be return NaN', function () {
+            let result = getFibonachi.fibonachiWithRecurtion(-6);
+            return expect(result).to.be.NaN;
+        });
+    });
+
+    describe("Entering a fractional number, should return NaN", function() {
+        it('6.5 should be return NaN', function () {
+            let result = getFibonachi.fibonachiWithRecurtion(6.5);
+            return expect(result).to.be.NaN;
+        });
+    });
+});
