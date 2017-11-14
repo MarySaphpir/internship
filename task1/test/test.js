cdec4d242a5fc01185c69db94f09026b5b59a80b
Mary <marysaphpir@gmail.com>
2017-11-14 09:36:34 +0200

    let to const

--- task1/test/test.js
+++ task1/test/test.js
@@ -1,7 +1,7 @@
-let chai = require('chai');
-let expect = require('chai').expect;
-let chaiAsPromised = require('chai-as-promised');
-let getFibonachi = require('../modulePatern');
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiAsPromised = require('chai-as-promised');
+const getFibonachi = require('../modulePatern');
 
 chai.use(chaiAsPromised);
 
@@ -9,26 +9,26 @@ describe("Testing promise", function() {
 
     describe("Entering number should return number of Fibonachi", function() {
         it('3 should be equal 2', function () {
-            let result = getFibonachi.fibonachiWithPromice(3);
+            const result = getFibonachi.fibonachiWithPromice(3);
             return expect(result).to.eventually.equal(2).and.to.be.finite;
         });
 
         it('6 should be equal 8', function () {
-            let result = getFibonachi.fibonachiWithPromice(6);
+            const result = getFibonachi.fibonachiWithPromice(6);
             return expect(result).to.eventually.equal(8);
         });
     });
 
     describe("Entering a negative number, should return NaN", function() {
         it('-6 should be return NaN', function () {
-            let result = getFibonachi.fibonachiWithPromice(-6);
+            const result = getFibonachi.fibonachiWithPromice(-6);
             return expect(result).to.be.NaN;
         });
     });
 
     describe("Entering a fractional number, should return NaN", function() {
         it('6.5 should be return NaN', function () {
-            let result = getFibonachi.fibonachiWithPromice(6.5);
+            const result = getFibonachi.fibonachiWithPromice(6.5);
             return expect(result).to.be.NaN;
         });
     });
@@ -39,26 +39,26 @@ describe("Testing function", function() {
 
     describe("Entering number should return number of Fibonachi", function() {
         it('3 should be equal 2', function () {
-            let result = getFibonachi.fibonachiWithRecurtion(3);
+            const result = getFibonachi.fibonachiWithRecurtion(3);
             return expect(result).to.be.equal(2).and.to.be.finite;
         });
 
         it('6 should be equal 8', function () {
-            let result = getFibonachi.fibonachiWithRecurtion(6);
+            const result = getFibonachi.fibonachiWithRecurtion(6);
             return expect(result).to.be.equal(8);
         });
     });
 
     describe("Entering a negative number, should return NaN", function() {
         it('-6 should be return NaN', function () {
-            let result = getFibonachi.fibonachiWithRecurtion(-6);
+            const result = getFibonachi.fibonachiWithRecurtion(-6);
             return expect(result).to.be.NaN;
         });
     });
 
     describe("Entering a fractional number, should return NaN", function() {
         it('6.5 should be return NaN', function () {
-            let result = getFibonachi.fibonachiWithRecurtion(6.5);
+            const result = getFibonachi.fibonachiWithRecurtion(6.5);
             return expect(result).to.be.NaN;
         });
     });
