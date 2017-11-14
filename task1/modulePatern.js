c91a9ce39a8671d5b641f98de64f6eef5563be65
Mary <marysaphpir@gmail.com>
2017-11-14 09:31:58 +0200

    fixed fibonachi

--- task1/modulePatern.js
+++ task1/modulePatern.js
@@ -3,6 +3,7 @@ let getFibonachi = (function () {
     function iterationFibonachi(n) {
         let firstNumber = 0;
         let secondNumber = 1;
+
         for (let i = 2; i <= n; i++) {
             let sum = firstNumber + secondNumber;
             firstNumber = secondNumber;
@@ -13,20 +14,22 @@ let getFibonachi = (function () {
 
     return {
         fibonachiWithRecurtion: function fibonachi(n) {
+            if (n < 0 || Math.round(n) != n) return NaN;
             return n <= 1 ? n : fibonachi(n - 2) + fibonachi(n - 1);
         },
 
         fibonachiWithIteration: function (n) {
+            checkinEntry(n);
             return iterationFibonachi(n)
         },
 
         fibonachiWithPromice: function fibonachi(n) {
+            if (n < 0 || Math.round(n) != n) return NaN;
             return Promise.resolve(iterationFibonachi(n));
-        }
+        },
+
     }
+
 }());
 
-let fibonachiWithPromise = getFibonachi.fibonachiWithPromice(6)
-    .then(number => console.log(number));
-
-console.log(fibonachiWithPromise);
+module.exports = getFibonachi;
