0fd05fce081d74119de6f76bf69f5ce929a25150
Mary <marysaphpir@gmail.com>
2017-11-29 10:50:01 +0200

    devide const.js

---
+++ weather-app/src/const/error.js
@@ -0,0 +1,2 @@
+export const SERVICE_ERROR = 'Error: The Geolocation service failed.';
+export const BROWSER_ERROR = 'Error: Your browser doesn\'t support geolocation.';
---
+++ weather-app/src/const/markerParam.js
@@ -0,0 +1,4 @@
+export const MIN_CIRCLE_RADIUS = 170000;
+export const MAX_CIRCLE_RADIUS = 280000;
+export const RADIUS_COEFFICIENT = 6000;
+export const AVERAGE_RADIUS = 6;
